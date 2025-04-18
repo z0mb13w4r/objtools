38b70c5fb49d484fc686a6a87f832b8707e38759
Kyle Wickens <kylewickens@gmail.com>
2025-04-18 16:22:09 +0100

    updated prerequisite.sh

--- prerequisite.sh
+++ prerequisite.sh
@@ -7,7 +7,12 @@ function aptcheck() {
   fi
 }
 
+aptcheck git
 aptcheck make
+aptcheck cmake
+aptcheck autoconf
+aptcheck libtool
+aptcheck pkg-config
 aptcheck gcc
 aptcheck g++
 aptcheck gcc-multilib
@@ -20,4 +25,21 @@ aptcheck gcc-aarch64-linux-gnu
 aptcheck g++-aarch64-linux-gnu
 aptcheck libssl-dev
 aptcheck binutils-dev
+
+mkdir libs
+cd libs
+
+git clone https://github.com/capstone-engine/capstone.git
+cd capstone
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+
+cd ..
+
+wget https://github.com/davea42/libdwarf-code/releases/download/v0.11.1/libdwarf-0.11.1.tar.xz
+tar xf libdwarf-0.11.1.tar.xz
+mkdir libdwarf-0.11.1/build
+cd libdwarf-0.11.1/build
+../configure
+make
 
