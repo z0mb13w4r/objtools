7f8e859afe5949850b4f505c70bef5b335ecc815
Kyle Wickens <kylewickens@gmail.com>
2025-04-23 14:08:39 -0400

    continue pe code

--- src/pecode.c
+++ src/pecode.c
@@ -244,8 +244,10 @@ DWORD get_dwordbyRVA(const pbuffer_t p, const int index, const uint64_t vaddr) {
 }
 
 const char* get_namebyord(const pbuffer_t p, const char* name, const pick_t x) {
-  if (0 == strcmp(name, "WS2_32.dll")) return strpick(zWS2_32, x);
-  else if (0 == strcmp(name, "KERNEL32.dll")) return strpick(zKERNEL32, x);
+  if (0 == strcasecmp(name, "KERNEL32.dll"))      return strpick(zKERNEL32, x);
+  else if (0 == strcasecmp(name, "OLEAUT32.dll")) return strpick(zOLEAUT32, x);
+  else if (0 == strcasecmp(name, "WS2_32.dll"))   return strpick(zWS2_32, x);
+  else if (0 == strcasecmp(name, "WSOCK32.dll"))  return strpick(zWSOCK32, x);
   return strpickunknown(x);
 }
 
