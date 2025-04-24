98f5dff419e5b2756b4f7d771778f999e6a33623
Kyle Wickens <kylewickens@gmail.com>
2025-04-24 07:52:29 +0100

    continue pe code

--- inc/pefunc.h
+++ inc/pefunc.h
@@ -3,6 +3,15 @@
 
 #include "buffer.h"
 
+typedef struct pefunc_s {
+  const char*   text;
+  const pick_t  ords;
+  const imode_t mode;
+} pefunc_t, *ppefunc_t;
+
+const char* funcpick(const ppefunc_t p, const pick_t x);
+const char* funcpicknull(const ppefunc_t p, const pick_t x);
+
 const char* get_namebyord(const pbuffer_t p, const char* name, const pick_t x);
 
 #endif
--- src/pefunc.c
+++ src/pefunc.c
@@ -3,11 +3,33 @@
 
 #include "static/ordinals.ci"
 
+const char* funcpick(const ppefunc_t p, const pick_t x) {
+  const char* s = funcpicknull(p, x);
+  if (NULL == s) {
+    return strpickunknown(x);
+  }
+
+  return s;
+}
+
+const char* funcpicknull(const ppefunc_t p, const pick_t x) {
+  if (p) {
+    for (ppefunc_t pp = p; 0 != pp->text; ++pp) {
+      if (pp->ords == x) {
+        return pp->text;
+      }
+    }
+  }
+
+  return NULL;
+}
+
 const char* get_namebyord(const pbuffer_t p, const char* name, const pick_t x) {
-  if (0 == strcasecmp(name, "KERNEL32.dll"))      return strpick(zKERNEL32, x);
-  else if (0 == strcasecmp(name, "OLEAUT32.dll")) return strpick(zOLEAUT32, x);
-  else if (0 == strcasecmp(name, "WS2_32.dll"))   return strpick(zWS2_32, x);
-  else if (0 == strcasecmp(name, "WSOCK32.dll"))  return strpick(zWSOCK32, x);
+  if (0 == strcasecmp(name, "KERNEL32.dll"))      return funcpick(zKERNEL32, x);
+  else if (0 == strcasecmp(name, "OLEAUT32.dll")) return funcpick(zOLEAUT32, x);
+  else if (0 == strcasecmp(name, "WS2_32.dll"))   return funcpick(zWS2_32, x);
+  else if (0 == strcasecmp(name, "WSOCK32.dll"))  return funcpick(zWSOCK32, x);
+
   return strpickunknown(x);
 }
 
