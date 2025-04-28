906ecd7dce8ac15b7fa5fc15db1f389c6078c348
Kyle Wickens <kylewickens@gmail.com>
2025-04-28 08:09:05 +0100

    continue bstr code

--- inc/bstring.h
+++ inc/bstring.h
@@ -5,7 +5,7 @@
 
 #include "defines.h"
 
-#define MODE_STRING              (MODE_PUT0('S') | MODE_PUT1('T') | MODE_PUT2('R'))
+#define MODE_BSTRING             (MODE_PUT0('S') | MODE_PUT1('T') | MODE_PUT2('R'))
 
 #define BSTRSPOS                 (0)
 #define BSTREPOS                 (~0)
@@ -17,9 +17,20 @@ typedef struct bstring_s {
 
 } bstring_t, *pbstring_t;
 
+bool_t isbstring(handle_t p);
+
 handle_t bstrmalloc();
 handle_t bstrmallocsize(const size_t size);
 handle_t bstrfree(handle_t p);
+
+size_t bstrlen(handle_t p);
+size_t bstrsize(handle_t p);
+
+handle_t bstrcpy(handle_t dst, handle_t src);
+handle_t bstrncpy(handle_t dst, handle_t src, size_t size);
+
+int bstrcmp(handle_t s1, handle_t s2);
+int bstrncmp(handle_t s1, handle_t s2, size_t size);
 
 #endif
 
--- src/bstring.c
+++ src/bstring.c
@@ -1,14 +1,21 @@
+#include <string.h>
+
 #include "buffer.h"
 #include "bstring.h"
 
+bool_t isbstring(handle_t p) {
+  return ismode(p, MODE_BSTRING) && 0 != CAST(pbstring_t, p)->data;
+}
+
+
 handle_t bstrmalloc() {
   handle_t p = xmalloc(sizeof(bstring_t));
-  return setmode(p, MODE_STRING);
+  return setmode(p, MODE_BSTRING);
 }
 
 handle_t bstrmallocsize(const size_t size) {
   handle_t p = bstrmalloc();
-  if (ismode(p, MODE_STRING)) {
+  if (ismode(p, MODE_BSTRING)) {
     pbstring_t p0 = CAST(pbstring_t, p);
     p0->data = xmalloc(size);
     p0->size = size;
@@ -18,12 +25,71 @@ handle_t bstrmallocsize(const size_t size) {
 }
 
 handle_t bstrfree(handle_t p) {
-  if (ismode(p, MODE_STRING)) {
+  if (ismode(p, MODE_BSTRING)) {
     free(CAST(pbstring_t, p)->data);
     free(p);
     return NULL;
   }
 
   return p;
+}
+
+size_t bstrlen(handle_t p) {
+  if (isbstring(p)) {
+    pbstring_t p0 = CAST(pbstring_t, p);
+    return strnlen(p0->data, p0->size);
+  }
+
+  return 0;
+}
+
+size_t bstrsize(handle_t p) {
+  return isbstring(p) ? CAST(pbstring_t, p)->size : 0;
+}
+
+handle_t bstrcpy(handle_t dst, handle_t src) {
+  if (isbstring(dst) && isbstring(src)) {
+    pbstring_t dst0 = CAST(pbstring_t, dst);
+    pbstring_t src0 = CAST(pbstring_t, src);
+    if (src0->size <= dst0->size) {
+      strncpy(dst0->data, src0->data, dst0->size);
+      return dst;
+    }
+  }
+
+  return 0;
+}
+
+handle_t bstrncpy(handle_t dst, handle_t src, size_t size) {
+  if (isbstring(dst) && isbstring(src)) {
+    pbstring_t dst0 = CAST(pbstring_t, dst);
+    pbstring_t src0 = CAST(pbstring_t, src);
+    if (size <= dst0->size) {
+      strncpy(dst0->data, src0->data, size);
+      return dst;
+    }
+  }
+
+  return 0;
+}
+
+int bstrcmp(handle_t s1, handle_t s2) {
+  if (isbstring(s1) && isbstring(s2)) {
+    pbstring_t s10 = CAST(pbstring_t, s1);
+    pbstring_t s20 = CAST(pbstring_t, s2);
+    return strncmp(s10->data, s20->data, MAX(s10->size, s20->size));
+  }
+
+  return -1;
+}
+
+int bstrncmp(handle_t s1, handle_t s2, size_t size) {
+  if (isbstring(s1) && isbstring(s2)) {
+    pbstring_t s10 = CAST(pbstring_t, s1);
+    pbstring_t s20 = CAST(pbstring_t, s2);
+    return strncmp(s10->data, s20->data, size);
+  }
+
+  return -1;
 }
 
