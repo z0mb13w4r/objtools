2361c7f8085740c5bdce49464ef2790c6789378a
Kyle Wickens <kylewickens@gmail.com>
2025-06-06 05:56:53 +0100

    continue dwarf code

--- src/ocdwarf.c
+++ src/ocdwarf.c
@@ -2,6 +2,7 @@
 
 #include "ocdwarf.h"
 #include "options.h"
+#include "ocdwarf-lines.h"
 #include "ocdwarf-printf.h"
 #include "ocdwarf-macroinfo.h"
 
@@ -36,39 +37,6 @@ static int ocdwarf_dodebug_info(handle_t p, handle_t s, handle_t d) {
 //  printf_text("Pointer Size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
 
   return n;
-}
-
-static int ocdwarf_dodebug_line(handle_t p, handle_t s, handle_t d) {
-  if (isopcode(p)) {
-    popcode_t p0 = CAST(popcode_t, p);
-    if (MODE_ISSET(p0->ocdump, OPTDEBUGELF_DEBUG_LINE)) {
-//      printf_text("Offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Length", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("DWARF Version", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Prologue Length", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Minimum Instruction Length", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Initial value of 'is_stmt'", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Line Base", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Line Range", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_text("Opcode Base", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE) | USE_EOL);
-//      printf_eol();
-//      printf_text("Opcodes", USE_LT | USE_SPACE | USE_COLON | USE_EOL);
-//      printf_eol();
-//      printf_text("The Directory Table", USE_LT | USE_SPACE);
-//      printf_nice(0x1b, USE_COLON | USE_FHEX8 | USE_OFFSET | USE_EOL);
-//      printf_eol();
-//      printf_text("The File Name Table", USE_LT | USE_SPACE);
-//      printf_nice(0xf8, USE_COLON | USE_FHEX8 | USE_OFFSET | USE_EOL);
-//      printf_eol();
-//      printf_text("Line Number Statements", USE_LT | USE_SPACE | USE_COLON | USE_EOL);
-//      printf_eol();
-    }
-
-    if (MODE_ISSET(p0->ocdump, OPTDEBUGELF_DEBUG_LINE_DECODED)) {
-    }
-  }
-
-  return 0;
 }
 
 static int ocdwarf_dodebug_str(handle_t p, handle_t s, handle_t d) { return 0; }
