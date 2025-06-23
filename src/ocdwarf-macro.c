cda5492bccc6168e10b804254c921eede5f89fc9
Kyle Wickens <kylewickens@gmail.com>
2025-06-23 06:46:16 +0100

    continue dwarf code

--- src/ocdwarf-macro.c
+++ src/ocdwarf-macro.c
@@ -132,26 +132,26 @@ int ocdwarf_debug_macro(handle_t p, handle_t s, handle_t d) {
       return OCDWARF_ERRCODE(x, n0);
     }
 
-      if (MODE_ISSET(oc->action, OPTPROGRAM_VERBOSE)) {
-        printf_text("CU header length", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(cu_header_length, USE_FHEX | USE_EOL);
-        printf_text("Version stamp", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(ws->cu_version_stamp, USE_DEC | USE_EOL);
-        printf_text("Abbrev offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(abbrev_offset, USE_DEC | USE_EOL);
-        printf_text("Address size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(address_size, USE_DEC | USE_EOL);
-        printf_text("Extension size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(extension_size, USE_DEC | USE_EOL);
-        printf_text("Type offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(type_offset, USE_FHEX | USE_EOL);
-        printf_text("CU offset size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(ws->cu_offset_size, USE_DEC | USE_EOL);
-        printf_text("CU next header offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(next_cu_header_offset, USE_FHEX | USE_EOL);
-        printf_text("CU header type", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-        printf_nice(header_cu_type, USE_FHEX | USE_EOL);
-      }
+    if (MODE_ISSET(oc->action, OPTPROGRAM_VERBOSE)) {
+      n += printf_text("CU header length", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(cu_header_length, USE_FHEX | USE_EOL);
+      n += printf_text("Version stamp", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(ws->cu_version_stamp, USE_DEC | USE_EOL);
+      n += printf_text("Abbrev offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(abbrev_offset, USE_DEC | USE_EOL);
+      n += printf_text("Address size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(address_size, USE_DEC | USE_EOL);
+      n += printf_text("Extension size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(extension_size, USE_DEC | USE_EOL);
+      n += printf_text("Type offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(type_offset, USE_FHEX | USE_EOL);
+      n += printf_text("CU offset size", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(ws->cu_offset_size, USE_DEC | USE_EOL);
+      n += printf_text("CU next header offset", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(next_cu_header_offset, USE_FHEX | USE_EOL);
+      n += printf_text("CU header type", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(header_cu_type, USE_FHEX | USE_EOL);
+    }
 
     Dwarf_Unsigned version = 0;
     Dwarf_Unsigned number_of_ops = 0;
