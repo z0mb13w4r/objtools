83eec77b53844df905b15d196130e0c06620b373
Kyle Wickens <kylewickens@gmail.com>
2025-06-29 09:06:34 +0100

    continue dwarf code

--- src/ocdwarf-abbrev.c
+++ src/ocdwarf-abbrev.c
@@ -12,6 +12,14 @@ int ocdwarf_debug_abbrev(handle_t p, handle_t s, handle_t d) {
   if (isopcode(p) && (isopshdr(s) || isopshdrNN(s))) {
     popcode_t oc = CAST(popcode_t, p);
 
+    Dwarf_Abbrev   abbrev = 0;
+    Dwarf_Unsigned offset = 0;
+    Dwarf_Unsigned length = 0;
+    Dwarf_Unsigned unused_entry_count = 0;
+
+    x = dwarf_get_abbrev(ocget(p, OPCODE_DWARF_DEBUG), offset, &abbrev, &length, &unused_entry_count, ocget(p, OPCODE_DWARF_ERROR));
+
+    ocdwarf_dealloc(p, abbrev, DW_DLA_ABBREV);
   }
 
   return OCDWARF_ERRCODE(x, n0);
