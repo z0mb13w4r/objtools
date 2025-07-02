cd13f24fde75f82095c2f3262af4837209ef81ae
Kyle Wickens <kylewickens@gmail.com>
2025-07-02 07:54:15 +0100

    continue dwarf code

--- src/ocdwarf-str.c
+++ src/ocdwarf-str.c
@@ -19,19 +19,34 @@ int ocdwarf_debug_str(handle_t p, handle_t s, handle_t d) {
                      &group_map_entry_count, ocget(p, OPCODE_DWARF_ERROR));
 
     if (group_count > 0) {
-      printf_text("Section Groups data", USE_LT | USE_EOL);
-      printf_text("Number of Elf-like sections", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-      printf_nice(section_count, USE_DEC | USE_EOL);
-      printf_text("Number of groups", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-      printf_nice(group_count, USE_DEC | USE_EOL);
-      printf_text("Group to print", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-      ocdwarf_printf_GNUM(p, selected_group, USE_EOL);
-      printf_text("Count of map entries", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
-      printf_nice(group_map_entry_count, USE_DEC | USE_EOL);
+      n += printf_text("Section Groups data", USE_LT | USE_EOL);
+      n += printf_text("Number of Elf-like sections", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(section_count, USE_DEC | USE_EOL);
+      n += printf_text("Number of groups", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(group_count, USE_DEC | USE_EOL);
+      n += printf_text("Group to print", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += ocdwarf_printf_GNUM(p, selected_group, USE_EOL);
+      n += printf_text("Count of map entries", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
+      n += printf_nice(group_map_entry_count, USE_DEC | USE_EOL);
 
       MALLOCA(Dwarf_Unsigned, sec_nums, group_map_entry_count);
       MALLOCA(Dwarf_Unsigned, group_nums, group_map_entry_count);
-      MALLOCA(char *, sec_names, group_map_entry_count);
+      MALLOCA(const char *, sec_names, group_map_entry_count);
+
+      x = dwarf_sec_group_map(ocget(p, OPCODE_DWARF_DEBUG), group_map_entry_count,
+                     group_nums, sec_nums, sec_names, ocget(p, OPCODE_DWARF_ERROR));
+
+      for (Dwarf_Unsigned i = 0; i < group_map_entry_count; ++i) {
+        if (i == 0) {
+          printf_text("[index]  group section", USE_LT | USE_TAB | USE_EOL);
+        }
+
+        n += printf_nice(i, USE_DEC5 | USE_SB);
+        n += printf_nice(group_nums[i], USE_DEC4);
+        n += printf_nice(sec_nums[i], USE_DEC4);
+        n += printf_text(sec_names[i], USE_LT | USE_SPACE);
+        n += printf_eol();
+      }
     }
 
     char* string = 0;
