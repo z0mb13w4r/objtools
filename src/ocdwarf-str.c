5a312fac15f32a31bc193c9f7e0f9dab1dc46b2c
Kyle Wickens <kylewickens@gmail.com>
2025-07-02 08:07:32 +0100

    continue dwarf code

--- src/ocdwarf-str.c
+++ src/ocdwarf-str.c
@@ -18,7 +18,7 @@ int ocdwarf_debug_str(handle_t p, handle_t s, handle_t d) {
     x = dwarf_sec_group_sizes(ocget(p, OPCODE_DWARF_DEBUG), &section_count, &group_count, &selected_group,
                      &group_map_entry_count, ocget(p, OPCODE_DWARF_ERROR));
 
-    if (group_count > 0) {
+    if (IS_DLV_OK(x) && (group_count > 0)) {
       n += printf_text("Section Groups data", USE_LT | USE_EOL);
       n += printf_text("Number of Elf-like sections", USE_LT | USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
       n += printf_nice(section_count, USE_DEC | USE_EOL);
@@ -36,16 +36,18 @@ int ocdwarf_debug_str(handle_t p, handle_t s, handle_t d) {
       x = dwarf_sec_group_map(ocget(p, OPCODE_DWARF_DEBUG), group_map_entry_count,
                      group_nums, sec_nums, sec_names, ocget(p, OPCODE_DWARF_ERROR));
 
-      for (Dwarf_Unsigned i = 0; i < group_map_entry_count; ++i) {
-        if (i == 0) {
-          printf_text("[index]  group section", USE_LT | USE_TAB | USE_EOL);
-        }
+      if (IS_DLV_OK(x)) {
+        for (Dwarf_Unsigned i = 0; i < group_map_entry_count; ++i) {
+          if (i == 0) {
+            printf_text("[index]  group section", USE_LT | USE_TAB | USE_EOL);
+          }
 
-        n += printf_nice(i, USE_DEC5 | USE_SB);
-        n += printf_nice(group_nums[i], USE_DEC4);
-        n += printf_nice(sec_nums[i], USE_DEC4);
-        n += printf_text(sec_names[i], USE_LT | USE_SPACE);
-        n += printf_eol();
+          n += printf_nice(i, USE_DEC5 | USE_SB);
+          n += printf_nice(group_nums[i], USE_DEC4);
+          n += printf_nice(sec_nums[i], USE_DEC4);
+          n += printf_text(sec_names[i], USE_LT | USE_SPACE);
+          n += printf_eol();
+        }
       }
     }
 
