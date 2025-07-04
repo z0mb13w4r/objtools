afd641668c445b960278ad4dbd0e7ed545c6ae76
Kyle Wickens <kylewickens@gmail.com>
2025-07-04 07:44:21 +0100

    continue dwarf code

--- src/ocdwarf-eh-frame.c
+++ src/ocdwarf-eh-frame.c
@@ -10,8 +10,6 @@ int ocdwarf_eh_frame(handle_t p, handle_t s, handle_t d) {
   int n = 0;
 
   if (isopcode(p) && (isopshdr(s) || isopshdrNN(s))) {
-    popcode_t oc = CAST(popcode_t, p);
-
     Dwarf_Cie *cie_data = NULL;
     Dwarf_Fde *fde_data = NULL;
     Dwarf_Signed cie_element_count = 0;
@@ -34,6 +32,33 @@ int ocdwarf_eh_frame(handle_t p, handle_t s, handle_t d) {
       ocdwarf_dealloc_error(p, NULL);
     }
 
+    for (Dwarf_Signed i = 0; i < fde_element_count; ++i) {
+      Dwarf_Addr     low_pc = 0;
+      Dwarf_Unsigned func_length = 0;
+      Dwarf_Small   *fde_bytes = NULL;
+      Dwarf_Unsigned fde_bytes_length = 0;
+      Dwarf_Off      fde_offset = 0;
+      Dwarf_Off      cie_offset = 0;
+      Dwarf_Signed   cie_index = 0;
+
+      Dwarf_Fde fde = fde_data[i];
+      x = dwarf_get_fde_range(fde, &low_pc, &func_length, &fde_bytes, &fde_bytes_length,
+                     &cie_offset, &cie_index, &fde_offset, ocget(p, OPCODE_DWARF_ERROR));
+      if (IS_DLV_NO_ENTRY(x)) break;
+      else if (IS_DLV_ERROR(x)) {
+        printf_e("dwarf_get_fde_range failed! - %d", x);
+        return OCDWARF_ERRCODE(x, n);
+      }
+
+      n += ocdwarf_printf_DEC(p, i, USE_NONE);
+
+      n += printf_text("cie offset", USE_LT | USE_TBLT);
+      n += printf_nice(cie_offset, USE_FHEX32);
+      n += printf_text("::cie index", USE_LT);
+      n += printf_nice(cie_index, USE_DEC | USE_TBRT);
+
+      n += printf_eol();
+    }
 
     ocdwarf_dealloc_fde_cie_list(p, cie_data, cie_element_count, fde_data, fde_element_count);
   }
