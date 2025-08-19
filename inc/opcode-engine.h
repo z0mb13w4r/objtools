f4f8eac77e78ef4fbc6272b57d7b4fac59b9c9f9
Kyle Wickens <kylewickens@gmail.com>
2025-08-19 05:35:24 +0100

    continue dissassembly

--- inc/opcode-engine.h
+++ inc/opcode-engine.h
@@ -5,9 +5,18 @@
 
 #define MODE_OCENGINE                  (MODE_PUT0('O') | MODE_PUT1('C') | MODE_PUT2('X'))
 
+typedef struct ocgroups_s {
+  uint64_t vaddr;
+
+  pocexamine_t examine;
+} ocgroups_t, *pocgroups_t;
+
 typedef struct ocengine_s {
   smode_t    mode;
   size_t     cpos, spos, epos;
+  size_t     size, sizemax;
+
+  pocgroups_t groups;
 
 } ocengine_t, *pocengine_t;
 
