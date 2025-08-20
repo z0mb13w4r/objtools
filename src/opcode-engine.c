7f66518f8c4ef75f60215e6dedf9138a93b0d37a
Kyle Wickens <kylewickens@gmail.com>
2025-08-20 06:10:35 +0100

    continue dissassembly

--- src/opcode-engine.c
+++ src/opcode-engine.c
@@ -33,25 +33,16 @@ handle_t oecreate_engine(handle_t p) {
 
 handle_t oegetbyaddr(handle_t p, const uint64_t vaddr, const imode_t mode) {
   if (isopcode(p)) {
-    pocengine_t p0 = ocget(p, OPCODE_ENGINE);
-    pocgroups_t q0 = p0 ? p0->groups : NULL;
-    if (q0) {
-      for (p0->cpos = 0; p0->cpos < p0->size; ++p0->cpos, ++q0) {
-        if (vaddr < q0->vaddr) break;
-        else if (vaddr == q0->vaddr) {
-          if (OPENGINE_GROUP == mode) {
-            return q0;
-          } else if (OPENGINE_EXAMINE == mode) {
-            return q0->examine;
-          }
-        }
-      }
+    handle_t p0 = oeseebyaddr(p, vaddr, mode);
+    if (NULL == p0 && OPENGINE_GROUP == mode) {
+      pocengine_t p1 = ocget(p, OPCODE_ENGINE);
+      pocgroups_t q1 = p1 && p1->groups ? p1->groups + p1->cpos : NULL;
 
-      if (OPENGINE_GROUP == mode && p0->size < p0->sizemax) {
-//printf("+++%ld %ld %lx+++", p0->cpos, p0->size + 1, vaddr);
-        ++p0->size;
-        q0->vaddr = vaddr;
-        return q0;
+      if (q1 && p1->size < p1->sizemax) {
+printf("+++%ld %ld %lx+++", p1->cpos, p1->size + 1, vaddr);
+        ++p1->size;
+        q1->vaddr = vaddr;
+        return q1;
       }
     }
   }
