538028032d6d375677a0288a0db378fee232ee36
Kyle Wickens <kylewickens@gmail.com>
2025-09-16 06:04:29 +0100

    continue debugging

--- inc/ocdwarf-engine.h
+++ inc/ocdwarf-engine.h
@@ -2,6 +2,9 @@
 #define __OCDWARF_ENGINE_H_
 
 #include "ocdwarf.h"
+#include "opcode-engine.h"
+
+handle_t ocdwarf_create(handle_t p, handle_t e);
 
 #endif
 
--- src/ocdwarf-engine.c
+++ src/ocdwarf-engine.c
@@ -1,2 +1,10 @@
 #include "ocdwarf-engine.h"
 
+handle_t ocdwarf_create(handle_t p, handle_t e) {
+  if (isopcode(p) && isocengine(e)) {
+    return e;
+  }
+
+  return NULL;
+}
+
