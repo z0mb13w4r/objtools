cb58d2aeb2d466d8f32177e4a076b38f72f27546
Kyle Wickens <kylewickens@gmail.com>
2025-09-24 05:30:27 +0100

    continue dissassembly

--- src/opcode-engine-got.c
+++ src/opcode-engine-got.c
@@ -1,5 +1,21 @@
 #include "opcode-engine-got.h"
 
+
+#include "printf.h"
+
+static void callback_sections(handle_t p, handle_t shdr, unknown_t param) {
+  printf_text(ocget_name(shdr), USE_LT);
+  printf_nice(ocget_size(shdr), USE_LHEX32);
+  printf_nice(ocget_vmaddress(shdr), USE_LHEX32);
+  printf_nice(ocget_lmaddress(shdr), USE_LHEX32);
+  printf_nice(ocget_position(shdr), USE_LHEX32);
+
+  uint64_t flags = ocget_flags(shdr);
+
+
+  printf_eol();
+}
+
 static handle_t execute_new(handle_t p, const uint64_t vaddr, const char* name) {
   if (isoengine(p)) {
     pocgroups_t g0 = oegetbyaddr(p, vaddr, OPENGINE_GROUP);
@@ -26,6 +42,8 @@ handle_t opcode_create(handle_t p, handle_t q) {
     execute_new(q, 0x1110, "malloc@plt");
     execute_new(q, 0x1120, "__printf_chk@plt");
     execute_new(q, 0x1130, "__ctype_b_loc@plt");
+
+    ocdo_sections(p, callback_sections, q);
     return q;
   }
 
