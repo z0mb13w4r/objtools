e5b7dfdb5f5164412bbe2a9e2cd59c82043eae11
Kyle Wickens <kylewickens@gmail.com>
2025-09-25 08:57:08 +0100

    continue dissassembly

--- src/opcode-engine-got.c
+++ src/opcode-engine-got.c
@@ -35,44 +35,21 @@ static void execute_section32(handle_t p, handle_t s, handle_t q) {
     uint64_t curr_vaddr = ocget_vmaddress(s);
     uint64_t prev_vaddr0 = 0;
     uint32_t prev_vaddr1 = 0;
-    printf("%s\n", ocget_name(s));
+    uint64_t prev_vaddr2 = ocget_vaddressbyname(p, ".got");
     for (uint64_t i = 0; i < ocget_size(s); ) {
       uint64_t siz = 1;
 
       if (0xf3 == pp[i + 0] && 0x0f == pp[i + 1] && 0x1e == pp[i + 2] && 0xfb == pp[i + 3]) { // endbr32
-        printf(" %04lx", curr_vaddr);
-        printf(" endbr32 f3 0f 1e fb\n");
-
         prev_vaddr0 = curr_vaddr;
         siz = 4;
       } else if (0xff == pp[i + 0] && 0xa3 == pp[i + 1]) { // jmp
         prev_vaddr1 = execute_u32(p, pp[i + 2], pp[i + 3], pp[i + 4], pp[i + 5]);
-
-        printf(" %04lx", curr_vaddr);
-        printf(" jmp ff a3 %04x\n", prev_vaddr1);
         siz = 6;
       } else if (0x66 == pp[i + 0] && 0x0f == pp[i + 1] && 0x1f == pp[i + 2] && 0x44 == pp[i + 3] && 0x00 == pp[i + 4] && 0x00 == pp[i + 5]) { // nopw
-        uint64_t this_vaddr = curr_vaddr + prev_vaddr1;
-
-        if (0x34 == prev_vaddr1) this_vaddr = 0x00003ff0;
-        else if (0x0c == prev_vaddr1) this_vaddr = 0x00003fc8;
-        else if (0x10 == prev_vaddr1) this_vaddr = 0x00003fcc;
-        else if (0x14 == prev_vaddr1) this_vaddr = 0x00003fd0;
-        else if (0x18 == prev_vaddr1) this_vaddr = 0x00003fd4;
-        else if (0x1c == prev_vaddr1) this_vaddr = 0x00003fd8;
-        else if (0x20 == prev_vaddr1) this_vaddr = 0x00003fdc;
-        else if (0x24 == prev_vaddr1) this_vaddr = 0x00003fe0;
-        else if (0x28 == prev_vaddr1) this_vaddr = 0x00003fe4;
-        else if (0x2c == prev_vaddr1) this_vaddr = 0x00003fe8;
-
+        uint64_t this_vaddr = prev_vaddr2 + prev_vaddr1;
         execute_new(q, prev_vaddr0, ocget_namebyvaddr(p, this_vaddr, NULL));
 
-        printf(" %04lx", curr_vaddr);
-        printf(" nopw 66 0f 1f 44 00 00 ** %04lx\n", this_vaddr);
-
         siz = 6;
-      } else {
-        printf(" %02x", pp[i]);
       }
 
       i += siz;
