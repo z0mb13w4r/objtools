1848d74945d0124240de67faa9310946788317f7
Kyle Wickens <kylewickens@gmail.com>
2025-10-14 07:22:38 +0100

    continue readelf decoding

--- src/ocdwarf-loc.c
+++ src/ocdwarf-loc.c
@@ -6,11 +6,10 @@
 int ocdwarf_debug_loc(handle_t p, handle_t s, handle_t d) {
   int x = DW_DLV_ERROR;
   int n0 = 0;
-  int n1 = 0;
+//  int n1 = 0;
 
   if (isopcode(p) && (isopshdr(s) || isopshdrNN(s))) {
-    popcode_t oc = ocget(p, OPCODE_THIS);
-
+//    popcode_t oc = ocget(p, OPCODE_THIS);
   }
 
   return OCDWARF_ERRCODE(x, n0);
