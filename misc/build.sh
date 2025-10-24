a060f84e1b9282532201659dab8b23c55e68dea8
Kyle Wickens <kylewickens@gmail.com>
2025-10-24 15:31:03 +0100

    updated build.sh

--- misc/build.sh
+++ misc/build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-NAME=objtools-0.0
-LOCALBIN=$NAME/usr/local/bin
-DEBIANBIN=$NAME/DEBIAN
-EXTERNBIN=../bin
-
+CUTBIN=/usr/bin/cut
 COPYBIN=/usr/bin/cp
+GREPBIN=/usr/bin/grep
 MAKEBIN=/usr/bin/make
 MKDIRBIN=/usr/bin/mkdir
 DPKGDEBBIN=/usr/bin/dpkg-deb
+
+EXTERNBIN=../bin
+
+VER=$($GREPBIN Version $EXTERNBIN/control | $CUTBIN -d ' ' -f 2)
+ARCH=$($GREPBIN Architecture $EXTERNBIN/control | $CUTBIN -d ' ' -f 2)
+
+NAME=objtools-$VER-$ARCH
+LOCALBIN=$NAME/usr/local/bin
+DEBIANBIN=$NAME/DEBIAN
 
 $MAKEBIN -f readpe-ng.mk all
 $MAKEBIN -f convert-ng.mk all
@@ -18,18 +24,18 @@ $MAKEBIN -f objhash-ng.mk all
 $MAKEBIN -f readelf-ng.mk all
 $MAKEBIN -f objdwarf-ng.mk all
 
-$MKDIRBIN -p $NAME
-$MKDIRBIN -p $LOCALBIN
-$MKDIRBIN -p $DEBIANBIN
+$MKDIRBIN -v -p  $NAME
+$MKDIRBIN -v -p $LOCALBIN
+$MKDIRBIN -v -p $DEBIANBIN
 
-$COPYBIN $EXTERNBIN/readpe-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/convert-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/objcopy-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/objdump-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/objhash-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/readelf-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/objdwarf-ng $LOCALBIN/
-$COPYBIN $EXTERNBIN/control $DEBIANBIN/
+$COPYBIN -v $EXTERNBIN/readpe-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/convert-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/objcopy-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/objdump-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/objhash-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/readelf-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/objdwarf-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/control $DEBIANBIN/
 
 $DPKGDEBBIN --build $NAME
 
