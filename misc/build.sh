9ea8a6078a7675a8955e733febf3389d2e91dabe
Kyle Wickens <kylewickens@gmail.com>
2025-10-25 16:27:04 +0100

    updated build.sh

--- misc/build.sh
+++ misc/build.sh
@@ -6,6 +6,8 @@ COPYBIN=/usr/bin/cp
 FINDBIN=/usr/bin/find
 GREPBIN=/usr/bin/grep
 MAKEBIN=/usr/bin/make
+SUDOBIN=/usr/bin/sudo
+CHOWNBIN=/usr/bin/chown
 MKDIRBIN=/usr/bin/mkdir
 MD5SUMBIN=/usr/bin/md5sum
 DPKGDEBBIN=/usr/bin/dpkg-deb
@@ -16,6 +18,7 @@ VER=$($GREPBIN Version $EXTERNBIN/control | $CUTBIN -d ' ' -f 2)
 ARCH=$($GREPBIN Architecture $EXTERNBIN/control | $CUTBIN -d ' ' -f 2)
 
 NAME=objtools-$VER-$ARCH
+USRBIN=$NAME/usr/bin
 LOCALBIN=$NAME/usr/local/bin
 DEBIANBIN=$NAME/DEBIAN
 
@@ -27,18 +30,20 @@ $MAKEBIN -f objhash-ng.mk all
 $MAKEBIN -f readelf-ng.mk all
 $MAKEBIN -f objdwarf-ng.mk all
 
-$MKDIRBIN -v -p  $NAME
-$MKDIRBIN -v -p $LOCALBIN
+$MKDIRBIN -v -p $NAME
+$MKDIRBIN -v -p $USRBIN
 $MKDIRBIN -v -p $DEBIANBIN
 
-$COPYBIN -v $EXTERNBIN/readpe-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/convert-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/objcopy-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/objdump-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/objhash-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/readelf-ng $LOCALBIN/
-$COPYBIN -v $EXTERNBIN/objdwarf-ng $LOCALBIN/
+$COPYBIN -v $EXTERNBIN/readpe-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/convert-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/objcopy-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/objdump-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/objhash-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/readelf-ng $USRBIN/
+$COPYBIN -v $EXTERNBIN/objdwarf-ng $USRBIN/
 $COPYBIN -v $EXTERNBIN/control $DEBIANBIN/
+
+$SUDOBIN $CHOWNBIN root:root $USRBIN/*
 
 $FINDBIN $NAME/usr/ -type f -exec $MD5SUMBIN '{}' \; | $SEDBIN "s/$NAME\///" > $DEBIANBIN/md5sums
 
