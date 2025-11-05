9ddb09746b673ddd7139677eb0e9d27939bfed00
Kyle Wickens <kylewickens@gmail.com>
2025-11-05 06:42:43 +0000

    Added functionality of demangling symbol names.

--- src/opcode-demangle.c
+++ src/opcode-demangle.c
@@ -1,6 +1,21 @@
+#include "options.h"
 #include "opcode-demangle.h"
 
 const char* opcode_demangle(handle_t p, const char* name) {
+  STATICA(char, name0, 1024);
+
+  if (isoptions(p)) {
+    poptions_t o = CAST(poptions_t, p);
+    if (MODE_ISANY(o->action, OPTPROGRAM_DEMANGLE)) {
+      int n = sprintf(name0, "%s", name);
+      if (n > 21) {
+        sprintf(name0 + 16, "[...]");
+      }
+
+      return name0;
+    }
+  }
+
   return name;
 }
 
