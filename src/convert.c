233b5f4cdc81d866ae5c8e6da9796cf9d2c6d066
Kyle Wickens <kylewickens@gmail.com>
2025-11-10 05:21:52 +0000

    Added functionality to brute force shl, shr and xor.

--- src/convert.c
+++ src/convert.c
@@ -1,3 +1,5 @@
+#include "decode.h"
+#include "printf.h"
 #include "convert.h"
 
 int dump_shlbrute8(const handle_t o, const unknown_t p, const size_t size) {
@@ -26,6 +28,20 @@ int dump_shrbrute16(const handle_t o, const unknown_t p, const size_t size) {
 
 int dump_xorbrute8(const handle_t o, const unknown_t p, const size_t size) {
   int n = 0;
+
+  if (p && size) {
+    for (int i = 1; i < 256; ++i) {
+      unknown_t p0 = cmalloc(p, size);
+      if (p0) {
+        xor8(p0, i, size, 0);
+
+        n += printf_text("Key =", USE_LT);
+        n += printf_nice(i, USE_LHEX8 | USE_COLON);
+        n += printf_sore(p0, size, USE_STR | USE_SPACE);
+        n += printf_eol();
+      }
+    }
+  }
 
   return n;
 }
