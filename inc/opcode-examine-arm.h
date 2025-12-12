136626e536b36c1fbbb05bb16610cf5b9a289dab
Kyle Wickens <kylewickens@gmail.com>
2025-12-12 07:09:22 +0000

    Updated functionality for Capstone disassembler (objdump-ng).

--- inc/opcode-examine-arm.h
+++ inc/opcode-examine-arm.h
@@ -1,11 +1,17 @@
 #ifndef __OPCODE_EXAMINE_ARM_H_
 #define __OPCODE_EXAMINE_ARM_H_
 
+#define OCINSNARM_BR               ((0x04) | OCINSN_OPERAND1)
+
 #define OCINSNARM_PUSH             ((0x0b) | OCINSN_OPERAND1)
 #define OCINSNARM_POP              ((0x0c) | OCINSN_OPERAND1)
 #define OCINSNARM_ADD              ((0x0d) | OCINSN_OPERAND2)
 #define OCINSNARM_SUB              ((0x18) | OCINSN_OPERAND2)
 #define OCINSNARM_LDR              ((0x20) | OCINSN_OPERAND2)
+
+#define OCINSNARM_LINK             U64MASK(16)
+
+#define OCINSNARM_BL               (OCINSNARM_BR | OCINSNARM_LINK)
 
 #endif
 
