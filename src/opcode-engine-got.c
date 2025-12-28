1292c975552c2c4d75a963012c60786763d47a8d
Kyle Wickens <kylewickens@gmail.com>
2025-12-28 07:47:10 +0000

    Updated functionality for Capstone disassembler (objdump-ng).

--- src/opcode-engine-got.c
+++ src/opcode-engine-got.c
@@ -86,10 +86,16 @@ static void execute_section32x86(handle_t p, handle_t s, handle_t q) {
 
 static char zADRP[] = "1iix0000iiiiiiiiiiiiiiiiiiiddddd"; // adrp Rd ADDR_ADRP
 static char zBR[]   = "x10x0110000xxxxxxxxxxxnnnnnxxxxx"; // br Rn
+
 static char zLDR0[] = "xx011100iiiiiiiiiiiiiiiiiiittttt"; // ldr Ft ADDR_PCREL19
 static char zLDR1[] = "xx111100x1xxxxxxxxxx10xxxxxttttt"; // ldr Ft ADDR_REGOFF
 static char zLDR2[] = "xx111100x1xiiiiiiiiiI1xxxxxttttt"; // ldr Ft ADDR_SIMM9
 static char zLDR3[] = "xxx11101x1iiiiiiiiiiiinnnnnttttt"; // ldr Ft ADDR_UIMM12
+static char zLDR4[] = "0x011000iiiiiiiiiiiiiiiiiiittttt"; // ldr Rt ADDR_PCREL19
+static char zLDR5[] = "1x111000011xxxxxxxxx10xxxxxttttt"; // ldr Rt ADDR_REGOFF
+static char zLDR6[] = "1x11100001xiiiiiiiiiI1xxxxxttttt"; // ldr Rt ADDR_SIMM9
+static char zLDR7[] = "1xx1100101iiiiiiiiiiiinnnnnttttt"; // ldr Rt ADDR_UIMM12
+
 static char zADD0[] = "x0001011xx0xxxxxxxxxxxnnnnnddddd"; // add Rd Rn Rm_SFT
 static char zADD1[] = "x00x0001SSiiiiiiiiiiiinnnnnddddd"; // add Rd_SP Rn_SP AIMM
 static char zADD2[] = "x00010110x1xxxxxxxxxxxnnnnnddddd"; // add Rd_SP Rn_SP Rm_EXT
@@ -98,7 +104,6 @@ static char zADD4[] = "xx001110xx1mmmmm100001nnnnnddddd"; // add Vd Vn Vm
 
 static uint32_t is01(handle_t p, const char* x, const size_t size, const int c, const uint32_t v) {
   if (x && 32 == size) {
-//printf("M");
     uint32_t xx = 0;
     for (size_t i = 0; i < size; ++i) {
       if (c == x[i]) {
@@ -150,9 +155,24 @@ static void execute_section64arm(handle_t p, handle_t s, handle_t q) {
 
     for (uint64_t i = 0; i < ocget_size(s); i += 4, curr_vaddr += 4) {
       uint32_t xx = execute_u32(p, pp[i + 0], pp[i + 1], pp[i + 2], pp[i + 3]);
-//printf("%03lx:%08x:%s:%s ", curr_vaddr, xx,
-//  is00(s, zADRP, sizeof(zADRP) - 1, xx) ? "adrp" : "no",
-//  is00(s, zBR, sizeof(zBR) - 1, xx) ? "br" : "no");
+//printf("%03lx:%08x ", curr_vaddr, xx);
+//printf("%s", is00(s, zADRP, sizeof(zADRP) - 1, xx) ? "adrp" : "");
+//printf("%s", is00(s, zBR, sizeof(zBR) - 1, xx) ? "br" : "");
+//printf("%s", is00(s, zLDR0, sizeof(zLDR0) - 1, xx) ? "ldr0" : "");
+//printf("%s", is00(s, zLDR1, sizeof(zLDR1) - 1, xx) ? "ldr1" : "");
+//printf("%s", is00(s, zLDR2, sizeof(zLDR2) - 1, xx) ? "ldr2" : "");
+//printf("%s", is00(s, zLDR3, sizeof(zLDR3) - 1, xx) ? "ldr3" : "");
+//printf("%s", is00(s, zLDR4, sizeof(zLDR4) - 1, xx) ? "ldr4" : "");
+//printf("%s", is00(s, zLDR5, sizeof(zLDR5) - 1, xx) ? "ldr5" : "");
+//printf("%s", is00(s, zLDR6, sizeof(zLDR6) - 1, xx) ? "ldr6" : "");
+//printf("%s", is00(s, zLDR7, sizeof(zLDR7) - 1, xx) ? "ldr7" : "");
+
+//printf("%s", is00(s, zADD0, sizeof(zADD0) - 1, xx) ? "add0" : "");
+//printf("%s", is00(s, zADD1, sizeof(zADD1) - 1, xx) ? "add1" : "");
+//printf("%s", is00(s, zADD2, sizeof(zADD2) - 1, xx) ? "add2" : "");
+//printf("%s", is00(s, zADD3, sizeof(zADD3) - 1, xx) ? "add3" : "");
+//printf("%s", is00(s, zADD4, sizeof(zADD4) - 1, xx) ? "add4" : "");
+
       if (is00(s, zADRP, sizeof(zADRP) - 1, xx)) { // adrp x16, 0x11000
 //printf("*");
         prev_vaddr0 = curr_vaddr;
