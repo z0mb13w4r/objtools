a58691b03dac639ebe7ed4879fffccc8163872a6
Kyle Wickens <kylewickens@gmail.com>
2026-02-14 14:18:43 +0000

    Updated prerequisite.sh script.

--- prerequisite.sh
+++ prerequisite.sh
@@ -29,23 +29,26 @@ aptcheck libtool
 aptcheck pkg-config
 aptcheck gcc
 aptcheck g++
-#aptcheck gcc-multilib
-#aptcheck g++-multilib
-#aptcheck gcc-mingw-w64
-#aptcheck g++-mingw-w64
-#aptcheck gcc-arm-linux-gnueabihf
-#aptcheck g++-arm-linux-gnueabihf
-#aptcheck gcc-aarch64-linux-gnu
-#aptcheck g++-aarch64-linux-gnu
-#aptcheck gcc-riscv64-linux-gnu
-#aptcheck g++-riscv64-linux-gnu
-#aptcheck gcc-mips-linux-gnu
-#aptcheck g++-mips-linux-gnu
+aptcheck gcc-multilib
+aptcheck g++-multilib
+aptcheck gcc-mingw-w64
+aptcheck g++-mingw-w64
+aptcheck gcc-arm-linux-gnueabihf
+aptcheck g++-arm-linux-gnueabihf
+aptcheck gcc-aarch64-linux-gnu
+aptcheck g++-aarch64-linux-gnu
+aptcheck gcc-riscv64-linux-gnu
+aptcheck g++-riscv64-linux-gnu
+aptcheck gcc-mips-linux-gnu
+aptcheck g++-mips-linux-gnu
 aptcheck libssl-dev
 aptcheck binutils-dev
 
-LIBDWARF=libdwarf-0.11.1
-LIBSSDEEP=ssdeep-2.14.1
+VERDWARF=0.11.1
+VERSSDEEP=2.14.1
+
+LIBDWARF=libdwarf-${VERDWARF}
+LIBSSDEEP=ssdeep-${VERSSDEEP}
 
 PWD=$(pwd)
 INC=${PWD}/inc
@@ -62,16 +65,16 @@ cmake --build build
 # ---- DWARF LIBRARY ----
 cdmk ${LIB}
 
-wget https://github.com/davea42/libdwarf-code/releases/download/v0.11.1/libdwarf-0.11.1.tar.xz
-tar xf libdwarf-0.11.1.tar.xz
+wget https://github.com/davea42/libdwarf-code/releases/download/v${VERDWARF}/libdwarf-${VERDWARF}.tar.xz
+tar xf libdwarf-${VERDWARF}.tar.xz
 cdmk ${LIBDWARF}/build
 ../configure
 make
 
 # ---- SDEEP LIBRARY ----
 cdmk ${LIB}
-wget https://github.com/ssdeep-project/ssdeep/releases/download/release-2.14.1/ssdeep-2.14.1.tar.gz
-tar xf tar xf ssdeep-2.14.1.tar.gz
+wget https://github.com/ssdeep-project/ssdeep/releases/download/release-${VERSSDEEP}/ssdeep-${VERSSDEEP}.tar.gz
+tar xf ssdeep-${VERSSDEEP}.tar.gz
 cdmk ${LIBSSDEEP}
 ./configure
 make
@@ -94,5 +97,5 @@ cdmv ${INC}
 
 ln -s ../libs/ssdeep/ ssdeep
 ln -s ../libs/capstone/include/ capstone
-ln -s ../libs/libdwarf-0.11.1/src/lib/libdwarf/ libdwarf
+ln -s ../libs/libdwarf-${VERDWARF}/src/lib/libdwarf/ libdwarf
 
