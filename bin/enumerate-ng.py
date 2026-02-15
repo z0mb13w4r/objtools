b7084cc7b9c711c68e67ef165b7630c7ef51caa3
Kyle Wickens <kylewickens@gmail.com>
2026-02-15 06:32:34 +0000

    Updated enumerate-ng.py script.

--- bin/enumerate-ng.py
+++ bin/enumerate-ng.py
@@ -8,7 +8,7 @@ def mk(msg):
 
 def go(msg,cmd):
   c = pexpect.spawn(cmd)
-  print(f'\033[31m[-] ' + msg + f':\033[00m\n' + c.readline().decode('utf-8')) #.rstrip('\n'))
+  print(f'\033[31m[-] ' + msg + f':\033[00m\n' + c.read().decode('utf-8')) #.rstrip('\n'))
 
 
 def ls(msg,cmd,params):
@@ -26,6 +26,19 @@ def system_info():
   go('Hostname', 'cat /etc/hostname')
 
 
+def user_info():
+  mk('USER/GROUP')
+  go('Current user/group info', 'id')
+  #go('Users that have previously logged onto the system', 'lastlog | grep -v "Never"')
+  go('Who else is logged on', 'w')
+  #go('Group memberships', '')
+  #go('It looks like we have some admin users', '')
+  go('It looks like we have password hashes in /etc/passwd!', 'grep -v "^[^:]*:[x]" /etc/passwd')
+  go('Contents of /etc/passwd', 'cat /etc/passwd')
+  go('We can read the shadow file!', 'cat /etc/shadow')
+
+
 if __name__ == '__main__':
   system_info()
+  user_info()
 
