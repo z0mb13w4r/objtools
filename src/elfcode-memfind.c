20d8a530c8d551f482c65d8e884d9f865c8d3ee6
Kyle Wickens <kylewickens@gmail.com>
2026-02-18 05:53:03 +0000

    Updated functionality for endian (readelf-ng).

--- src/elfcode-memfind.c
+++ src/elfcode-memfind.c
@@ -18,44 +18,52 @@ handle_t ecapply_relocs(handle_t p, handle_t q, const int index) {
 //printf("missing[REL]\n");
           } else if (SHT_RELA == s0->sh_type) {
             MEMSTACK(Elf64_Shdr, dx);
-            Elf64_Rela *r0 = _get64byshdr(q, s0);
             Elf64_Shdr *d0 = ecget_shdr64byindex(q, dx, s0->sh_link);
-            if (r0 && d0) {
-              for (size_t j = 0; j < cnt; ++j, ++r0) {
-                MEMSTACK(Elf64_Sym, mx);
-                Elf64_Sym *m0 = ecconvert_sym64(q, mx, getp(q, d0->sh_offset + (ELF64_R_SYM(r0->r_info) * d0->sh_entsize), d0->sh_entsize));
+            if (d0) {
+              handle_t q0 = fget64byshdr(q, s0);
+              if (q0) {
+                for (size_t j = 0; j < cnt; ++j) {
+                  MEMSTACK(Elf32_Rel, rx);
+                  Elf64_Rela *r0 = ecconvert_rela64(q, rx, fgetp(q0, sizeof(Elf64_Rela)));
+                  if (r0) {
+                    MEMSTACK(Elf64_Sym, mx);
+                    Elf64_Sym *m0 = ecconvert_sym64(q, mx, getp(q, d0->sh_offset + (ELF64_R_SYM(r0->r_info) * d0->sh_entsize), d0->sh_entsize));
 //printf("offset[RELA] = 0x%lx|0x%lx:0x%lx:0x%lx|0x%lx|0x%lx\n", r0->r_offset, r0->r_info, ELF64_R_TYPE(r0->r_info), ELF64_R_SYM(r0->r_info), m0->st_value, r0->r_addend);
-                if (isused(get_RELTYPESYM32(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (m0 && m0->st_value) {
+                    if (isused(get_RELTYPESYM32(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (m0 && m0->st_value) {
 //printf("sym32 0x%lx\n", m0->st_value);
-                    fsetu32byoffset(p, r0->r_offset, m0->st_value);
-                  }
-                } else if (isused(get_RELTYPESYM64(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (m0 && m0->st_value) {
+                        fsetu32byoffset(p, r0->r_offset, m0->st_value);
+                      }
+                    } else if (isused(get_RELTYPESYM64(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (m0 && m0->st_value) {
 //printf("sym64 0x%lx\n", m0->st_value);
-                    fsetu64byoffset(p, r0->r_offset, m0->st_value);
-                  }
-                } else if (isused(get_RELTYPESHEX8(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (r0->r_addend) {
+                        fsetu64byoffset(p, r0->r_offset, m0->st_value);
+                      }
+                    } else if (isused(get_RELTYPESHEX8(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (r0->r_addend) {
 //printf("shex8 0x%lx\n", r0->r_addend);
-                    fsetu8byoffset(p, r0->r_offset, r0->r_addend);
-                  }
-                } else if (isused(get_RELTYPESHEX16(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (r0->r_addend) {
+                        fsetu8byoffset(p, r0->r_offset, r0->r_addend);
+                      }
+                    } else if (isused(get_RELTYPESHEX16(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (r0->r_addend) {
 //printf("shex16 0x%lx\n", r0->r_addend);
-                    fsetu16byoffset(p, r0->r_offset, r0->r_addend);
-                  }
-                } else if (isused(get_RELTYPESHEX32(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (r0->r_addend) {
+                        fsetu16byoffset(p, r0->r_offset, r0->r_addend);
+                      }
+                    } else if (isused(get_RELTYPESHEX32(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (r0->r_addend) {
 //printf("shex32 0x%lx\n", r0->r_addend);
-                    fsetu32byoffset(p, r0->r_offset, r0->r_addend);
-                  }
-                } else if (isused(get_RELTYPESHEX64(q), ELF64_R_TYPE(r0->r_info))) {
-                  if (r0->r_addend) {
+                        fsetu32byoffset(p, r0->r_offset, r0->r_addend);
+                      }
+                    } else if (isused(get_RELTYPESHEX64(q), ELF64_R_TYPE(r0->r_info))) {
+                      if (r0->r_addend) {
 //printf("shex64 0x%lx\n", r0->r_addend);
-                    fsetu64byoffset(p, r0->r_offset, r0->r_addend);
+                        fsetu64byoffset(p, r0->r_offset, r0->r_addend);
+                      }
+                    }
                   }
                 }
+
+                ffree(q0);
               }
             }
           } else if (SHT_RELR == s0->sh_type) {
