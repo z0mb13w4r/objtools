55e49ecb64dbd8359a64984257c81b27098d846c
Kyle Wickens <kylewickens@gmail.com>
2026-03-08 13:51:36 +0000

    Updated enumerate-ng.py script.

--- bin/enumerate-ng.py
+++ bin/enumerate-ng.py
@@ -205,7 +205,9 @@ def ask_info(args):
 
 if __name__ == '__main__':
   p = argparse.ArgumentParser(prog='enumerate-ng', description='Used for enumeration and privilege escalation to harden devices or understanding how an adversary obtained persistence, privilege escalation, or lateral movement within a network.')
-  p.add_argument('-u', '--username', help='user password for sudo checks (insecure).')
+  p.add_argument('-u', '--username', help='username for checks.')
+  p.add_argument('-p', '--password', help='user password for sudo checks (insecure).')
+  p.add_argument('-d', '--path', default='', help='path to the firmware image.')
   p.add_argument('-m', '--more', action='store_true', help='more thorough tests.')
   p.add_argument('--system', action='store_true', help='system information.')
   p.add_argument('--user', action='store_true', help='user & group information.')
