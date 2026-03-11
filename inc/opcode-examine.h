dea6646455043e78d9e3bdb389bb67d5e2941fef
Kyle Wickens <kylewickens@gmail.com>
2026-03-11 06:11:36 +0000

    Updated functionality for Capstone disassembler x86 (objdump-ng).

--- inc/opcode-examine.h
+++ inc/opcode-examine.h
@@ -86,10 +86,9 @@
 #define OCINSN_09               U64MASK(25)
 #define OCINSN_10               U64MASK(26)
 #define OCINSN_11               U64MASK(27)
-#define OCINSN_12               U64MASK(28)
-#define OCINSN_13               U64MASK(29)
-#define OCINSN_14               U64MASK(30)
 
+#define OCINSN_ALIGNED          U64MASK(29)
+#define OCINSN_UNALIGNED        U64MASK(30)
 #define OCINSN_LINK             U64MASK(31)
 #define OCINSN_ADDRESS          U64MASK(32)
 #define OCINSN_IMMEDIATE        U64MASK(33)
