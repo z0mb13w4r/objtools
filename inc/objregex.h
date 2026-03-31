c991e987ff7577954816648aa59365856fb4b78a
Kyle Wickens <kylewickens@gmail.com>
2026-03-31 06:23:51 +0100

    Updated regex functionality (detect-ng).

--- inc/objregex.h
+++ inc/objregex.h
@@ -11,14 +11,17 @@ typedef struct re_s {
   smode_t   mode;
 
   unknown_t data;
+  unknown_t groups;
 
 } re_t, *pre_t;
 
 
-bool_t isre(handle_t p);
+bool_t isregex(handle_t p);
 
 handle_t rmalloc(const char *pattern);
 handle_t rfree(handle_t p);
+
+bool_t regex_match(handle_t p, const char *match);
 
 #endif
 
--- src/objregex.c
+++ src/objregex.c
@@ -1,7 +1,9 @@
 #include "memuse.h"
 #include "objregex.h"
 
-bool_t isre(handle_t p) {
+#define REGEXGROUP_MAXSIZE           (10)
+
+bool_t isregex(handle_t p) {
   return ismode(p, MODE_REGEX);
 }
 
@@ -9,11 +11,13 @@ handle_t rmalloc(const char *pattern) {
   pre_t p0 = xmalloc(sizeof(re_t), MODE_HEAP);
   if (p0) {
     p0->data = xmalloc(sizeof(regex_t), MODE_HEAP);
+    p0->groups = xmalloc(REGEXGROUP_MAXSIZE * sizeof(regmatch_t), MODE_HEAP);
 
     if (regcomp(p0->data, pattern, REG_EXTENDED)) {
       return setmode(p0, MODE_REGEX);
     }
 
+    xfree(p0->groups);
     xfree(p0->data);
     xfree(p0);
   }
@@ -22,17 +26,29 @@ handle_t rmalloc(const char *pattern) {
 }
 
 handle_t rfree(handle_t p) {
-  if (isre(p)) {
+  if (isregex(p)) {
     pre_t p0 = CAST(pre_t, p);
     if (p0->data) {
       regfree(p0->data);
-      xfree(p0->data);
     }
 
+    xfree(p0->groups);
+    xfree(p0->data);
     xfree(p0);
     return NULL;
   }
 
   return p;
+}
+
+bool_t regex_match(handle_t p, const char *match) {
+  if (isregex(p)) {
+    pre_t p0 = CAST(pre_t, p);
+    if (p0->data && p0->groups) {
+      return !regexec(p0->data, match, REGEXGROUP_MAXSIZE, p0->groups, 0);
+    }
+  }
+
+  return FALSE;
 }
 
