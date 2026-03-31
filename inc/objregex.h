939c3df360b2cfdc677bd8bf5e2e2d751ed4a0dc
Kyle Wickens <kylewickens@gmail.com>
2026-03-31 06:07:00 +0100

    Updated regex functionality (detect-ng).

--- inc/objregex.h
+++ inc/objregex.h
@@ -1,7 +1,24 @@
 #ifndef __OBJREGEX_H_
 #define __OBJREGEX_H_
 
+#include <regex.h>
+
 #include "defines.h"
+
+#define MODE_REGEX             (MODE_PUT0('R') | MODE_PUT1('E') | MODE_PUT2('G') | MODE_PUT3('X'))
+
+typedef struct re_s {
+  smode_t   mode;
+
+  unknown_t data;
+
+} re_t, *pre_t;
+
+
+bool_t isre(handle_t p);
+
+handle_t rmalloc(const char *pattern);
+handle_t rfree(handle_t p);
 
 #endif
 
--- src/objregex.c
+++ src/objregex.c
@@ -1,2 +1,38 @@
+#include "memuse.h"
 #include "objregex.h"
+
+bool_t isre(handle_t p) {
+  return ismode(p, MODE_REGEX);
+}
+
+handle_t rmalloc(const char *pattern) {
+  pre_t p0 = xmalloc(sizeof(re_t), MODE_HEAP);
+  if (p0) {
+    p0->data = xmalloc(sizeof(regex_t), MODE_HEAP);
+
+    if (regcomp(p0->data, pattern, REG_EXTENDED)) {
+      return setmode(p0, MODE_REGEX);
+    }
+
+    xfree(p0->data);
+    xfree(p0);
+  }
+
+  return NULL;
+}
+
+handle_t rfree(handle_t p) {
+  if (isre(p)) {
+    pre_t p0 = CAST(pre_t, p);
+    if (p0->data) {
+      regfree(p0->data);
+      xfree(p0->data);
+    }
+
+    xfree(p0);
+    return NULL;
+  }
+
+  return p;
+}
 
