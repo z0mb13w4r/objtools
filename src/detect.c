164233a5d818f4df826654392f6563fa56d7ca37
Kyle Wickens <kylewickens@gmail.com>
2026-04-02 08:41:38 +0100

    Updated signature detection (detect-ng).

--- src/detect.c
+++ src/detect.c
@@ -20,8 +20,10 @@ int detect_create(const pbuffer_t p, const poptions_t o) {
     printf_i("opened: %s", o->inpname1);
 
     handle_t inp2 = fcalloc(inp1->data, inp1->size, MEMFIND_NOCHUNKSIZE);
+    handle_t out1 = fcalloc(NULL, inp1->size, MEMFIND_NOCHUNKSIZE);
     handle_t out2 = fcalloc(NULL, inp1->size, MEMFIND_NOCHUNKSIZE);
-    if (inp2 && out2) {
+
+    if (inp2 && out1 && out2) {
       pre_t r1 = rmalloc("\\[(.*)\\]");
       pre_t r2 = rmalloc("signature = (([0-9A-F?]{2} ?)+)");
       pre_t r3 = rmalloc("ep_only = (false|true)");
@@ -33,29 +35,39 @@ int detect_create(const pbuffer_t p, const poptions_t o) {
           int x1 = regex_match(r1, p2);
           if (x1) {
             if (regex_isvalue(r1, 1)) {
-              fsetchunk(out2, DETECT_NAME, regex_getsize(r1, 1) + 1);
-              fsetp(out2, regex_getvalue(r1, 1), regex_getsize(r1, 1));
 printf("$NAME$ \"%s\" is found at position %d to %d (%ld).\n",
             regex_getvalue(r1, 1), regex_getso(r1, 1), regex_geteo(r1, 1) - 1, regex_getsize(r1, 1));
+
+              fsetchunk(out2, DETECT_NAME, regex_getsize(r1, 1) + 1);
+              fsetp(out2, regex_getvalue(r1, 1), regex_getsize(r1, 1));
             }
           } else {
             int x2 = regex_match(r2, p2);
             if (x2) {
               if (regex_isvalue(r1, 1)) {
-                fsetchunk(out2, DETECT_SIGNATURE, regex_getsize(r2, 1) + 1);
-
 printf("$SIGNATURE$ \"%s\" is found at position %d to %d (%ld).\n",
               regex_getvalue(r2, 1), regex_getso(r2, 1), regex_geteo(r2, 1) - 1, regex_getsize(r2, 1));
+
+                freset(out1);
+                for (size_t i = 0; i <= regex_getsize(r2, 1); ++i) {
+                  fsetu8(out1, regex_getvalue(r2, 1)[i]);
+                }
+                fsetu8(out1, 0);
+
+                fshrink(out1);
+                fsetchunk(out2, DETECT_SIGNATURE, fgetsize(out1));
+                fsetp(out2, fget(out1), fgetsize(out1));
               }
             } else {
               int x3 = regex_match(r3, p2);
               if (x3) {
                 if (regex_isvalue(r1, 1)) {
-                  fsetchunk(out2, DETECT_FLAG, regex_getsize(r2, 1) + 1);
-                  fsetu32(out2, isbool(regex_getvalue(r3, 1), regex_getsize(r3, 1)) ? DETECT_EP_ONLY : DETECT_NONE);
 printf("$FLAG$ \"%s\" is found at position %d to %d (%ld:%s).\n",
                 regex_getvalue(r3, 1), regex_getso(r3, 1), regex_geteo(r3, 1) - 1, regex_getsize(r3, 1),
                 isbool(regex_getvalue(r3, 1), regex_getsize(r3, 1)) ? "y" : "n");
+
+                  fsetchunk(out2, DETECT_FLAG, regex_getsize(r2, 1) + 1);
+                  fsetu32(out2, isbool(regex_getvalue(r3, 1), regex_getsize(r3, 1)) ? DETECT_EP_ONLY : DETECT_NONE);
                 }
               }
             }
@@ -73,6 +85,7 @@ printf("$FLAG$ \"%s\" is found at position %d to %d (%ld:%s).\n",
 
     ffree(inp2);
     ffree(out2);
+    ffree(out1);
 
     bfree(inp1);
   } else {
