8eafdf6207f7c84d914a82596118b680b7a97923
Kyle Wickens <kylewickens@gmail.com>
2026-04-05 11:30:02 +0100

    Updated signature detection (detect-ng).

--- src/signatures.c
+++ src/signatures.c
@@ -8,7 +8,8 @@
 #define FLAG_ISLO            U32MASK(1)
 
 int signature_scan(handle_t p, const char* data, const size_t datasize, const uint32_t mode) {
-  if (isfind(p) && data /*&& datasize < fgetsize(p)*/) {
+  const size_t safesize = (datasize + 1) / 2;
+  if (isfind(p) && data && safesize < fgetsize(p)) {
     for (size_t i = 0; i < datasize; i += 2) {
       uint32_t state = FLAG_NONE;
 
@@ -39,16 +40,25 @@ int signature_scan(handle_t p, const char* data, const size_t datasize, const ui
 }
 
 int signature_pecode(handle_t p, const char* data, const size_t datasize, const uint32_t mode) {
-  if (isPE(p) && data && datasize) {
+  const size_t filesize = bgetsize(p);
+  const size_t safesize = (datasize + 1) / 2;
+  if (isPE(p) && data && safesize < bgetsize(p)) {
     handle_t p1 = fcalloc(bget(p), bgetsize(p), MEMFIND_NOBLOCKSIZE);
     if (p1) {
-      fmove(p1, peget_addressofentrypoint(p));
-
-      int count = signature_scan(p1, data, datasize, mode);
+      int maxcount = 0;
+      if (MODE_ISANY(mode, SIGNATURE_EP_ONLY)) {
+        fmove(p1, peget_addressofentrypoint(p));
+        maxcount = signature_scan(p1, data, datasize, mode);
+      } else {
+        for (int i = 0; i < (filesize - safesize); ++i) {
+          fmove(p1, i);
+          maxcount = MAX(maxcount, signature_scan(p1, data, datasize, mode));
+        }
+      }
 
       ffree(p1);
 
-      return count;
+      return maxcount;
     }
   }
 
