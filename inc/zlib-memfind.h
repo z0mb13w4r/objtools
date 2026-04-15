e80ed8cf651985b390dee7a1231b0da7e4539a96
Kyle Wickens <kylewickens@gmail.com>
2026-04-15 17:31:57 +0100

    Added zlib functionality (readelf-ng).

--- inc/zlib-memfind.h
+++ inc/zlib-memfind.h
@@ -1,9 +1,14 @@
 #ifndef __ZLIB_MEMFIND_H_
 #define __ZLIB_MEMFIND_H_
 
+#define ZLIB_NO_COMPRESSION        (1)
+#define ZLIB_BEST_SPEED            (2)
+#define ZLIB_BEST_COMPRESSION      (3)
+#define ZLIB_DEFAULT_COMPRESSION   (4)
+
 #include "memfind.h"
 
-handle_t zlib_compress(handle_t p, handle_t q, const int level);
+handle_t zlib_compress(handle_t p, handle_t q, const imode_t mode);
 handle_t zlib_decompress(handle_t p, handle_t q);
 
 #endif
--- src/zlib-memfind.c
+++ src/zlib-memfind.c
@@ -1,6 +1,8 @@
+#include <zlib.h>
+
 #include "zlib-memfind.h"
 
-handle_t zlib_compress(handle_t p, handle_t q, const int level) {
+handle_t zlib_compress(handle_t p, handle_t q, const imode_t mode) {
   return NULL;
 }
 
