46cab49057d782e0d8354cfc715601d77b95b792
Kyle Wickens <kylewickens@gmail.com>
2026-04-18 14:33:37 +0100

    Updated strings-ng.py script.

--- bin/strings-ng.py
+++ bin/strings-ng.py
@@ -33,8 +33,9 @@ BINBIGLIST=r'aria2c\|arp\|ash\|awk\|base64\|bash\|busybox\|cat\|chmod\|chown\|cp
 ## file\|
 EXEBIGLIST=r'reboot\|poweroff\|halt\|netstat\|systemd'
 DIRBIGLIST=r'/dev\|/var\|/tmp\|/usr\|/etc\|/bin\|/sbin\|/mnt\|/root\|/boot\|/home\|/media\|/opt\|/proc\|/lib\|/pts'
+INTBIGLIST=r'USER-AGENT\|HOST\|Cookie\|POST\|GET\|url\|http\|https\|udp\|dns'
 TXTBIGLIST=r'ptm\|tty\|group\|passwd\|shells\|xterm'
-EXTBIGLIST=r'gz\|zip\|7z\|pub'
+EXTBIGLIST=r'gz\|zip\|7z\|pub\|pdf\|doc\|docx\|png\|jpg\|jpeg'
 
 def mk(msg):
   print('\033[33m### ' + msg + ' ' + '#'*(52 - len(msg)) + '\033[00m')
@@ -71,7 +72,7 @@ def xz(args, color, msg, c0, c1=None):
     if msg:
       print(color + f'[-] ' + msg + f':\033[00m')
 
-    print(r)
+    print(r, end='')
     return r
 
   return None
@@ -118,28 +119,40 @@ def all_info(args):
 
 def dir_info(args):
   if args.directories:
-    mk('DIRECTORY')
+    mk('DIRECTORIES')
     gz(args, 'strings ' + args.name + ' | grep -w "' + DIRBIGLIST + '" | grep -v "' + BINBIGLIST + '" | grep -v "' + EXEBIGLIST + '"')
+    print('')
 
 
 def ext_info(args):
   if args.extensions:
-    mk('EXTENSIONS')
+    mk('POSSSIBLE EXTENSIONS')
     gz(args, 'strings ' + args.name + ' | grep -w "' + EXTBIGLIST + '"')
     #gz(args, 'strings ' + args.name + ' | grep -w "' + TXTBIGLIST + '"')
     #go(args, 'Found some interesting text', 'strings ' + args.name + ' | grep -E "[[:alnum:]]+"')
+    print('')
 
 
 def sys_info(args):
   if args.privilege:
-    mk('PRIVILEGE ESCALATION')
+    mk('POSSIBLE PRIVILEGE ESCALATION')
     gz(args, 'strings ' + args.name + ' | grep -w "' + BINBIGLIST + '"')
+    print('')
 
 
 def bin_info(args):
   if args.binaries:
     mk('BINARIES')
     gz(args, 'strings ' + args.name + ' | grep -w "' + EXEBIGLIST + '"')
+    print('')
+
+
+def int_info(args):
+  if args.internet:
+    mk('INTERNET')
+    gz(args, 'strings ' + args.name + ' | grep -w "' + INTBIGLIST + '"')
+    gz(args, 'strings ' + args.name + ' | egrep "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}" | grep -v "' + INTBIGLIST + '"')
+    print('')
 
 
 if __name__ == '__main__':
@@ -156,11 +169,12 @@ if __name__ == '__main__':
   p.add_argument('-p', '--privilege', action='store_true', help='search for possible privilege escalation information.')
   p.add_argument('-e', '--extensions', action='store_true', help='search for possible extensions.')
   p.add_argument('-d', '--directories', action='store_true', help='search for possible directories.')
+  p.add_argument('-i', '--internet', action='store_true', help='search for possible internet triggers.')
   p.add_argument('--norun', action='store_true', help=argparse.SUPPRESS)
   g = p.parse_args()
 
-  if not(g.privilege or g.binaries or g.extensions or g.directories):
-    g.binaries = g.privilege = g.extensions = g.directories = True
+  if not(g.privilege or g.binaries or g.extensions or g.directories or g.internet):
+    g.binaries = g.privilege = g.extensions = g.directories = g.internet = True
 
   if g.version:
     print(PROGRAM_NAME + ' v' + VERSION_VALUE + '\n')
@@ -174,3 +188,4 @@ if __name__ == '__main__':
     bin_info(g)
     sys_info(g)
     ext_info(g)
+    int_info(g)
