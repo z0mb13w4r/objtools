78577fe23292ad133a2fb8c21265f4e03ed71d9b
Kyle Wickens <kylewickens@gmail.com>
2026-04-27 08:17:09 +0100

    Updated strings-ng.py script.

--- bin/strings-ng.py
+++ bin/strings-ng.py
@@ -32,12 +32,13 @@ LICENSE_TEXT = '''COPYRIGHT
 BINBIGLIST=r'aria2c\|arp\|ash\|awk\|base64\|bash\|busybox\|cat\|chmod\|chown\|cp\|csh\|curl\|cut\|dash\|date\|dd\|diff\|dmsetup\|docker\|ed\|emacs\|env\|expand\|expect\|find\|flock\|fmt\|fold\|ftp\|gawk\|gdb\|gimp\|git\|grep\|head\|ht\|iftop\|ionice\|ip$\|irb\|jjs\|jq\|jrunscript\|ksh\|ld.so\|ldconfig\|less\|logsave\|lua\|make\|man\|mawk\|more\|mv\|mysql\|nano\|nawk\|nc\|netcat\|nice\|nl\|nmap\|node\|od\|openssl\|perl\|pg\|php\|pic\|pico\|python\|readelf\|rlwrap\|rpm\|rpmquery\|rsync\|ruby\|run-parts\|rvim\|scp\|script\|sed\|setarch\|sftp\|sh\|shuf\|socat\|sort\|sqlite3\|ssh$\|start-stop-daemon\|stdbuf\|strace\|systemctl\|tail\|tar\|taskset\|tclsh\|tee\|telnet\|tftp\|time\|timeout\|ul\|unexpand\|uniq\|unshare\|vi\|vim\|watch\|wget\|wish\|xargs\|xxd\|zip\|zsh'
 ## file\|
 EXEBIGLIST=r'reboot\|poweroff\|halt\|netstat\|systemd\|ftpget\|ftpput\|watchdog\|adb\|adbd\|docker\|dockerd'
-DIRBIGLIST=r'/dev\|/var\|/tmp\|/usr\|/etc\|/bin\|/sbin\|/mnt\|/root\|/boot\|/home\|/media\|/opt\|/proc\|/lib\|/pts\|/data\|/local'
+DIRBIGLIST=r'/sys\|kernel\|/dev\|/var\|/tmp\|/usr\|/etc\|/bin\|/sbin\|/mnt\|/root\|/boot\|/home\|/media\|/opt\|/proc\|/lib\|/pts\|/data\|/local\|Program Files\|System32\|Users'
 INTBIGLIST=r'USER-AGENT\|HOST\|Cookie\|POST\|GET\|url\|http\|https\|udp\|dns\|google'
 TXTBIGLIST=r'ptm\|tty\|group\|passwd\|shells\|xterm'
 EXTBIGLIST=r'\.gz$\|\.zip$\|\.7z$\|\.pub$\|\.pdf$\|\.doc$\|\.docx$\|\.png$\|\,jpg$\|\.jpeg$\|\.go$\|\.exe$\|\.ps1$\|\.sh$\|\.py$'
-SECBIGLIST=r'\.shstrtab\|\.init\|\.text\|\.fini\|.rodata\|\.ctors\|\.dtors\|\.data\|\.bss'
-MGCBINLIST=r'pid\|upx\|abcdefghijklmnopqrstuvw.*012345678\|\.x86\|\.x86_64\|\.arm\|\.arm5\|\.arm6\|\.arm7\|\.mips\|\.mipsel\|\.sh4\|\.ppc\|TeamSpeak'
+SECBIGLIST=r'\.shstrtab\|\.init\|\.text\|\.fini\|.rodata\|\.ctors\|\.dtors\|\.data\|\.bss\|\.noptrdata\|\.elfdata\|\.typelink\|\.itablink\|\.gosymtab\|\.gopclntab'
+MGCBINLIST=r'pid\|upx\|abcdefghijklmnopqrstuvw.*012345678\|\.x86\|\.x86_64\|\.arm\|\.arm5\|\.arm6\|\.arm7\|\.mips\|\.mipsel\|\.sh4\|\.ppc\|TeamSpeak\|build\|net\|os\|io\|syscall\|crypto\|unicode\|runtime'
+
 
 def pk(args):
   if g.version:
