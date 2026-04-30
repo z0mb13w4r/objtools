25ab0dac2c971f9739ef3318c6be8c58ed51d34e
Kyle Wickens <kylewickens@gmail.com>
2026-04-30 07:43:17 +0100

    Added printf_size (readpe-ng).

--- inc/printf-custom.h
+++ inc/printf-custom.h
@@ -5,6 +5,7 @@
 #include "options.h"
 
 int printf_fork(const poptions_t o, const pconvert_t p, const pconvert_t q, const pick_t x, const imode_t mode);
+int printf_size(const poptions_t o, const uint64_t v, const imode_t mode);
 
 #endif
 
--- src/printf-custom.c
+++ src/printf-custom.c
@@ -13,3 +13,16 @@ int printf_fork(const poptions_t o, const pconvert_t p, const pconvert_t q, cons
   return n;
 }
 
+int printf_size(const poptions_t o, const uint64_t v, const imode_t mode) {
+  int n = 0;
+
+  if (MODE_ISANY(o->action, OPTPROGRAM_VERBOSE)) {
+    n += printf_nice(v, USE_BYTES);
+    n += printf_nice(v, USE_FHEX | GET_FLAG(mode));
+  } else {
+    n += printf_nice(v, mode);
+  }
+
+  return n;
+}
+
