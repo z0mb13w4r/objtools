51aad779a005049e6889113c5dd045607f685f58
Kyle Wickens <kylewickens@gmail.com>
2026-06-12 07:13:34 +0100

    Added functionality for archive (arcode).

--- src/arcode.c
+++ src/arcode.c
@@ -46,7 +46,7 @@ handle_t ecget_archive(const pbuffer_t p, const int index) {
       struct ar_hdr* p0 = getp(p, SARMAG, HDRSIZE);
       if (p0) {
         size_t size1 = decb(p0->ar_size, sizeof(p0->ar_size));
-        if (0 < size1) {
+        if (ISSIZE(size1)) {
           unknown_t p1 = getp(p, SARMAG, HDRSIZE + size1);
           if (p1) {
             return fmalloc(p1, HDRSIZE + size1, MEMFIND_NOBLOCKSIZE);
@@ -57,18 +57,33 @@ handle_t ecget_archive(const pbuffer_t p, const int index) {
       size_t size = bgetsize(p) - SARMAG;
       handle_t p0 = fmalloc(getp(p, SARMAG, size), size, MEMFIND_NOBLOCKSIZE);
       if (p0) {
+        size_t cpos = fgetcpos(p0);
         struct ar_hdr* p1 = fgetp(p0, HDRSIZE);
         int i = 1;
         while (i < index && p1) {
 //printf("size = %ld\n", decb(p1->ar_size, sizeof(p1->ar_size)));
           fstep(p0, decb(p1->ar_size, sizeof(p1->ar_size)));
+
+          cpos = fgetcpos(p0);
           p1 = fgetp(p0, HDRSIZE);
           ++i;
         }
 
-        ffree(p0);
-      }
+        handle_t p2 = NULL;
+        if (p1 && (index == i)) {
+          size_t size1 = decb(p1->ar_size, sizeof(p1->ar_size));
+          if (ISSIZE(size1)) {
+            unknown_t p3 = fmove(p0, cpos);
+            if (p3) {
+//printf("size = %d:%ld\n", index, size1);
+              p2 = fmalloc(p3, HDRSIZE + size1, MEMFIND_NOBLOCKSIZE);
+            }
+          }
+        }
 
+        ffree(p0);
+        return p2;
+      }
     }
   }
 
