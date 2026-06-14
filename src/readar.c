845a9d974920f3c34b8b212e080ee96aec55b4c6
Kyle Wickens <kylewickens@gmail.com>
2026-06-14 09:06:37 +0100

    Added functionality for archive (arcode).

--- src/readar.c
+++ src/readar.c
@@ -9,19 +9,25 @@ static const int MAXSIZE = 36;
 static int dump_achive0(const pbuffer_t p, const poptions_t o, const int index, const size_t size) {
   int n = 0;
 
+  const imode_t USE_FHEXNN = MODE_ISANY(size, MEMFIND_64BIT) ? USE_FHEX64 : USE_FHEX32;
+
   handle_t p0 = ecget_archive(p, index, size);
   if (p0) {
+    n += printf_text("ARCHIVE BODY", USE_LT | USE_COLON | USE_EOL);
+
     struct ar_hdr* p1 = fgetp(p0, sizeof(struct ar_hdr));
     if (p1) {
-printf("size = %ld\n", decb(p1->ar_size, sizeof(p1->ar_size)));
-//printf_sore(fget(p0), fgetsize(p0), USE_HEXDUMP);
       size_t count = fgetuNN(p0);
-printf("count = %ld\n", count);
+      n += printf_text("Count", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+      n += printf_nice(count, USE_DEC | USE_EOL);
+
+      n += printf_text("INDICES", USE_TAB | USE_COLON | USE_EOL);
       for (size_t i = 0; i < count; ++i) {
         if (fisokNN(p0)) {
-printf("%lx\n", fgetuNN(p0));
+          n += printf_text("Index[]", USE_TAB2 | USE_COLON | SET_PAD(MAXSIZE - 1));
+          n += printf_nice(fgetuNN(p0), USE_FHEXNN | USE_EOL);
         } else {
-printf("error has occurred\n");
+          printf_e("missing indices from archive");
           break;
         }
       }
@@ -42,22 +48,22 @@ static int dump_archive(const pbuffer_t p, const poptions_t o, const int index) 
   struct ar_hdr* p0 = ecget_ahdr(p, index);
   if (p0) {
     n += printf_text("ARCHIVE HEADER", USE_LT | USE_COLON | USE_EOL);
-    n += printf_text("Name", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("Name", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_name, sizeof(p0->ar_name), USE_STR | USE_EOL);
 
-    n += printf_text("Date", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("Date", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_date, sizeof(p0->ar_date), USE_STR | USE_EOL);
 
-    n += printf_text("UID", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("UID", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_uid, sizeof(p0->ar_uid), USE_STR | USE_EOL);
 
-    n += printf_text("GID", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("GID", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_gid, sizeof(p0->ar_gid), USE_STR | USE_EOL);
 
-    n += printf_text("Mode", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("Mode", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_mode, sizeof(p0->ar_mode), USE_STR | USE_EOL);
 
-    n += printf_text("Size", USE_TAB | USE_COLON | SET_PAD(MAXSIZE - 1));
+    n += printf_text("Size", USE_TAB | USE_COLON | SET_PAD(MAXSIZE));
     n += printf_sore(p0->ar_size, sizeof(p0->ar_size), USE_STR | USE_EOL);
 
     if (0 == xstrncmp(p0->ar_name, "/               ", sizeof(p0->ar_name))) {
