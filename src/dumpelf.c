d87e3a64c5e570542152e73e50cfb29ee158f47e
Kyle Wickens <kylewickens@gmail.com>
2026-07-09 05:48:29 +0100

    Updated functionality for coredumps (coredump-ng).

--- src/dumpelf.c
+++ src/dumpelf.c
@@ -2596,6 +2596,8 @@ static int dump_notes6Ax64(const pbuffer_t p, const poptions_t o, const uint64_t
   n += printf_text("SID", USE_LT | USE_COLON | USE_TAB2);
   n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  char           pr_fname[16];
+  n += printf_text("FILENAME", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_sore(fgetp(notes, 16), 16, USE_STR | USE_EOL);
 //  char           pr_psargs[80];
 
   return n;
