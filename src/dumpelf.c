042309e1beb793b0f361c4665755e2b397978bf3
Kyle Wickens <kylewickens@gmail.com>
2026-07-14 05:53:05 +0100

    Updated functionality for coredumps (coredump-ng).

--- src/dumpelf.c
+++ src/dumpelf.c
@@ -2825,33 +2825,38 @@ static int dump_notes7(const pbuffer_t p, const poptions_t o, const uint64_t e_m
   n += printf_text("TRAP NUMBER", USE_LT | USE_COLON | USE_TAB2);
   n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  pid_t    si_pid;       /* Sending process ID */
-    n += printf_text("PROCESS ID", USE_LT | USE_COLON | USE_TAB2);
-    n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
+  n += printf_text("PROCESS ID", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  uid_t    si_uid;       /* Real user ID of sending process */
-    n += printf_text("USER ID", USE_LT | USE_COLON | USE_TAB2);
-    n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
+  n += printf_text("USER ID", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  int      si_status;    /* Exit value or signal */
-    n += printf_text("STATUS", USE_LT | USE_COLON | USE_TAB2);
-    n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
+  n += printf_text("STATUS", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  clock_t  si_utime;     /* User time consumed */
-    n += printf_text("UTIME", USE_LT | USE_COLON | USE_TAB2);
-    n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
+  n += printf_text("UTIME", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  clock_t  si_stime;     /* System time consumed */
-    n += printf_text("STIME", USE_LT | USE_COLON | USE_TAB2);
-    n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
+  n += printf_text("STIME", USE_LT | USE_COLON | USE_TAB2);
+  n += printf_nice(fgetu32(notes), USE_FHEX32 | USE_EOL);
 //  union sigval si_value; /* Signal value */
 //  int      si_int;       /* POSIX.1b signal */
 //  void    *si_ptr;       /* POSIX.1b signal */
+  n += dump_notesNN(p, o, e_machine, n_descsz, notes, "PTR");
 //  int      si_overrun;   /* Timer overrun count; POSIX.1b timers */
 //  int      si_timerid;   /* Timer ID; POSIX.1b timers */
 //  void    *si_addr;      /* Memory location which caused fault */
+  n += dump_notesNN(p, o, e_machine, n_descsz, notes, "ADDR");
 //  long     si_band;      /* Band event (was int in glibc 2.3.2 and earlier) */
 //  int      si_fd;        /* File descriptor */
 //  short    si_addr_lsb;  /* Least significant bit of address (since Linux 2.6.32) */
 //  void    *si_lower;     /* Lower bound when address violation occurred (since Linux 3.19) */
+  n += dump_notesNN(p, o, e_machine, n_descsz, notes, "LOWER");
 //  void    *si_upper;     /* Upper bound when address violation occurred (since Linux 3.19) */
+  n += dump_notesNN(p, o, e_machine, n_descsz, notes, "UPPER");
 //  int      si_pkey;      /* Protection key on PTE that caused fault (since Linux 4.6) */
 //  void    *si_call_addr; /* Address of system call instruction (since Linux 3.5) */
+  n += dump_notesNN(p, o, e_machine, n_descsz, notes, "CALL ADDR");
 //  int      si_syscall;   /* Number of attempted system call (since Linux 3.5) */
 //  unsigned int si_arch;  /* Architecture of attempted system call (since Linux 3.5) */
 
