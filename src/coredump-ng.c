10a32fd2625a6631478649c142830371d3097d14
Kyle Wickens <kylewickens@gmail.com>
2026-07-22 05:32:17 +0100

    Updated options (coredump-ng).

--- src/coredump-ng.c
+++ src/coredump-ng.c
@@ -14,7 +14,7 @@ static int usage(poptions_t o, const char* name, const args_t args[], const int 
   n += usage_synopsis0(o, name, args);
 //  n += usage_synopsis1(o, name, zDEBUGELFARGS, zREADELFARGS0, zREADELFARGS1);
 //  n += usage_synopsis2(o, name, zDISASSEMBLEARGS, zREADELFARGS2, zREADELFARGS3);
-  n += usage_objname(o, name, "elffile");
+  n += usage_objname(o, name, "objfiles");
   n += printf_eol();
   n += usage_description(o, name, args, zELFDESCRIPTION);
   n += usage_options0(o, name, args);
