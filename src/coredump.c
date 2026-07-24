45cf3bf272860bc6158e507b55d9b8bb3cbcb228
Kyle Wickens <kylewickens@gmail.com>
2026-07-24 06:57:55 +0100

    Updated options (coredump-ng).

--- src/coredump.c
+++ src/coredump.c
@@ -10,45 +10,53 @@ int coredump(const pbuffer_t p, const pbuffer_t q, const poptions_t o) {
     if (MODE_ISANY(o->action, OPTCOREELF_FILEHEADER))           dumpelf_elfheader(p, o);
 
     if (isELF32(p)) {
-      Elf32_Ehdr *ehdr = ecget_ehdr32(p, xmalloc(sizeof(Elf32_Ehdr), MODE_HEAP));
-      if (ehdr) {
-        if (MODE_ISANY(o->action, OPTCOREELF_FILEHEADER))       dumpelf_fileheader32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK))      dumpelf_sectionheaders32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONGROUPS))    dumpelf_sectiongroups32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS))   dumpelf_programheaders32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_DYNAMIC))          dumpelf_dynamic32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_RELOCS))           dumpelf_relocs32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_UNWIND))           dumpelf_unwind32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SYMBOLS))          dumpelf_symbols32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_HISTOGRAM))        dumpelf_histogram32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_VERSION))          dumpelf_version32(p, o, ehdr);
-        if (o->actions)                                         dumpelf_actions32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_NOTES))            dumpelf_notes32(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_ARCHSPECIFIC))     dumpelf_archspecific32(p, o, ehdr);
-        if (MODE_ISANY(o->ocdump, OPTDWARF_DEBUGGING))          dumpelf_dwarf32(p, o, ehdr);
+      Elf32_Ehdr *e1 = ecget_ehdr32(p, xmalloc(sizeof(Elf32_Ehdr), MODE_HEAP));
+      Elf32_Ehdr *e2 = isELF32(q) ? ecget_ehdr32(q, xmalloc(sizeof(Elf32_Ehdr), MODE_HEAP)) : NULL;
+      if (e1) {
+        if (MODE_ISANY(o->action, OPTCOREELF_FILEHEADER))           dumpelf_fileheader32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK))          dumpelf_sectionheaders32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK) && e2)    dumpelf_sectionheaders32(q, o, e2);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONGROUPS))        dumpelf_sectiongroups32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS))       dumpelf_programheaders32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS) && e2) dumpelf_programheaders32(q, o, e2);
+        if (MODE_ISANY(o->action, OPTCOREELF_DYNAMIC))              dumpelf_dynamic32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_RELOCS))               dumpelf_relocs32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_UNWIND))               dumpelf_unwind32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SYMBOLS))              dumpelf_symbols32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_HISTOGRAM))            dumpelf_histogram32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_VERSION))              dumpelf_version32(p, o, e1);
+        if (o->actions)                                             dumpelf_actions32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_NOTES))                dumpelf_notes32(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_ARCHSPECIFIC))         dumpelf_archspecific32(p, o, e1);
+        if (MODE_ISANY(o->ocdump, OPTDWARF_DEBUGGING))              dumpelf_dwarf32(p, o, e1);
       }
 
-      xfree(ehdr);
+      xfree(e1);
+      xfree(e2);
     } else if (isELF64(p)) {
-      Elf64_Ehdr *ehdr = ecget_ehdr64(p, xmalloc(sizeof(Elf64_Ehdr), MODE_HEAP));
-      if (ehdr) {
-        if (MODE_ISANY(o->action, OPTCOREELF_FILEHEADER))       dumpelf_fileheader64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK))      dumpelf_sectionheaders64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONGROUPS))    dumpelf_sectiongroups64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS))   dumpelf_programheaders64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_DYNAMIC))          dumpelf_dynamic64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_RELOCS))           dumpelf_relocs64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_UNWIND))           dumpelf_unwind64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_SYMBOLS))          dumpelf_symbols64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_HISTOGRAM))        dumpelf_histogram64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_VERSION))          dumpelf_version64(p, o, ehdr);
-        if (o->actions)                                         dumpelf_actions64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_NOTES))            dumpelf_notes64(p, o, ehdr);
-        if (MODE_ISANY(o->action, OPTCOREELF_ARCHSPECIFIC))     dumpelf_archspecific64(p, o, ehdr);
-        if (MODE_ISANY(o->ocdump, OPTDWARF_DEBUGGING))          dumpelf_dwarf64(p, o, ehdr);
+      Elf64_Ehdr *e1 = ecget_ehdr64(p, xmalloc(sizeof(Elf64_Ehdr), MODE_HEAP));
+      Elf64_Ehdr *e2 = isELF64(q) ? ecget_ehdr64(q, xmalloc(sizeof(Elf64_Ehdr), MODE_HEAP)) : NULL;
+      if (e1) {
+        if (MODE_ISANY(o->action, OPTCOREELF_FILEHEADER))           dumpelf_fileheader64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK))          dumpelf_sectionheaders64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONMASK && e2))    dumpelf_sectionheaders64(q, o, e2);
+        if (MODE_ISANY(o->action, OPTCOREELF_SECTIONGROUPS))        dumpelf_sectiongroups64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS))       dumpelf_programheaders64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_PROGRAMHEADERS) && e2) dumpelf_programheaders64(q, o, e2);
+        if (MODE_ISANY(o->action, OPTCOREELF_DYNAMIC))              dumpelf_dynamic64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_RELOCS))               dumpelf_relocs64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_UNWIND))               dumpelf_unwind64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_SYMBOLS))              dumpelf_symbols64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_HISTOGRAM))            dumpelf_histogram64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_VERSION))              dumpelf_version64(p, o, e1);
+        if (o->actions)                                             dumpelf_actions64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_NOTES))                dumpelf_notes64(p, o, e1);
+        if (MODE_ISANY(o->action, OPTCOREELF_ARCHSPECIFIC))         dumpelf_archspecific64(p, o, e1);
+        if (MODE_ISANY(o->ocdump, OPTDWARF_DEBUGGING))              dumpelf_dwarf64(p, o, e1);
       }
 
-      xfree(ehdr);
+      xfree(e1);
+      xfree(e2);
     }
   } else {
     printf_e("not an ELF file - it has the wrong magic bytes at the start.");
